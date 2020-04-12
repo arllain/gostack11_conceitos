3b0e213c82d759dd0b5dcead0c0163748b58c084
arllainm <arllain@gmail.com>
2020-04-12 19:01:08 -0300

    Conceitos Front-end com ReactJS

---
+++ frontend/public/script.js
@@ -0,0 +1,10 @@
+/**
+ * Babel: Converter (transpilar) código do React para um código que o brower
+ * entenda
+ *
+ * Webpack: Para cada tipo de arquivo(.js, .css, .png) eu vou converter o código
+ * de um maneira diferente
+ *
+ * Loaders: babel-loader, css-loader, image-loader. Utilizado pelo babel
+ *
+ */
